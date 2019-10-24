79bd0748cfe6fc921736fe420353b51f99dc6df8
Aaliyah Johnson <aaliyahjohnson@BGL-MAC-008905.bgl.financial.local>
2019-10-24 11:04:13 +0100

    connect css to erb file

--- app.rb
+++ app.rb
@@ -2,11 +2,13 @@ require "sinatra/base"
 require './lib/grocery'
 require "pg"
 
+
 class GroceryApp < Sinatra::Base
 
   get ('/') do
     @grocery = Grocery.all
-    "Welcome to the Grocery App #{@grocery}!"
+    "Welcome to the Grocery App!"
+    erb :index
   end
 
   run! if app_file == $0
--- lib/grocery.rb
+++ lib/grocery.rb
@@ -5,7 +5,16 @@ class Grocery
     connection = PG.connect(dbname: 'grocerydb')
 
     result = connection.exec("SELECT * FROM groceries;")
-    result.map { |item| item['items']}
+    result.map { |groceries|
+      Grocery.new(id: groceries['id'], item: groceries['items'], price: groceries['price']) }
 
+  end
+
+  attr_reader :id, :item, :price
+
+  def initialize(id:, item:, price:)
+    @id = id
+    @item = item
+    @price = price
   end
 end
