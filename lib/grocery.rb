c186319f4379791e5a95b3d9aff190dd72b202e3
Aaliyah Johnson <aaliyahjohnson@BGL-MAC-008905.bgl.financial.local>
2019-10-24 09:44:06 +0100

    displaying items in browser

--- lib/grocery.rb
+++ lib/grocery.rb
@@ -4,7 +4,8 @@ class Grocery
   def self.all
     connection = PG.connect(dbname: 'grocerydb')
 
-    result = connection.exec("SELECT * FROM groceries")
-    result.map { |item| item['items'] }
+    result = connection.exec("SELECT * FROM groceries;")
+    result.map { |item| item['items']}
+
   end
 end
